b22c0fa5a0ca7d639bff6baa50af9584a0d68b7d
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-08-15 02:20:59 +0530

    .

--- src/Components/GenreComp.jsx
+++ src/Components/GenreComp.jsx
@@ -1,18 +1,33 @@
 import { useState,useEffect } from "react";
 
 const GenreComp = ({genres,setGenres,selectedGenres,setSelectedGenres}) => {
-    const changeSelection = (index) => {
+    const changeSelection = async (index) => {
         const changedGenre = genres.map((gen, i) =>
-            i === index ? { ...gen, selected: !gen.selected } : gen
+            i === index 
+            ? { ...gen, selected: !gen.selected } 
+            : gen
           );
-        setGenres(changedGenre); 
+        await setGenres(changedGenre); 
     }
+    // const updatedSelectedGenres=(index)=>{
+    //     let tempselected=[Array(7).fill(null)];
+    //     if(genres[index].selected===true){
+    //         console.log(genres[index]);
+    //         tempselected[index]=genres[index];
+    //     }
+    //     else{
+    //         tempselected[index]=null;
+    //     }
+    //     setSelectedGenres(tempselected);
+    // }
     
     return ( 
         <div >
             <ul className="genre-names">
                 {genres.map((gen,index)=>(
-                    <li key={index}  onClick={()=>{changeSelection(index)}} className={`${gen.selected ===false ? "unselected-genre" : "selected-genre"}`}>
+                    <li key={index}  onClick={()=>{
+                        changeSelection(index);
+                        }} className={`${gen.selected ===false ? "unselected-genre" : "selected-genre"}`}>
                             {gen.name}
                     </li>
                 ))}
